b3e40957dba5b0a8c8b7520dd783c683686c365f
SelivanovaLera <lera.selivanova@gmail.com>
2014-12-26 14:41:07 +0300

    initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Minesweeper Game=new Minesweeper();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
